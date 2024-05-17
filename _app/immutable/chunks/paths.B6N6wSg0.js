2ebcb1f491a75dceb6157f8f21e0bc0006a9f9c2
lukasVirtual <gerlinskilukas@gmail.com>
2024-05-17 22:11:01 +0200

    Updates

---
+++ _app/immutable/chunks/paths.B6N6wSg0.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_165no8o)==null?void 0:s.base)??"/emeraldbuisness";var e;const o=((e=globalThis.__sveltekit_165no8o)==null?void 0:e.assets)??a;export{o as a,a as b};
--- _app/immutable/chunks/paths.DDaWJut4.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_q10goo)==null?void 0:s.base)??"/emeraldbuisness";var e;const o=((e=globalThis.__sveltekit_q10goo)==null?void 0:e.assets)??a;export{o as a,a as b};
