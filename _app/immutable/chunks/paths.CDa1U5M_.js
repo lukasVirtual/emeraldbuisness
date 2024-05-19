58d648e43cd552f093d5bdc89f0d6c3fd9090b8b
lukasVirtual <gerlinskilukas@gmail.com>
2024-05-19 23:28:08 +0200

    Updates

---
+++ _app/immutable/chunks/paths.CDa1U5M_.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1wzqt0c)==null?void 0:s.base)??"/emeraldbuisness";var e;const a=((e=globalThis.__sveltekit_1wzqt0c)==null?void 0:e.assets)??t;export{a,t as b};
